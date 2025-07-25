060f19682328d5b79e4bee6ec9f3406c665c460f
Naman Sinha <aman.sinha5224@gmail.com>
2025-07-25 22:30:50 +0530

    update for loop

--- 11_ForLoops.py
+++ 11_ForLoops.py
@@ -17,3 +17,13 @@ print("\n")
 fruits = ["apple", "mango", "peach", "orange", "watermelon"]
 for fruit in fruits:
     print(fruit.capitalize())
+
+# For-else Loop
+# 'else' block will be executed once all the iterations in loop is executed, if break in between than code in 'else' will not be executed
+
+l = [1, 2, 3, 4]
+for i in range(len(l)):
+    print(l[i], end = " ")
+    if(i == 2): break
+else:
+    print("Reached Limit")
