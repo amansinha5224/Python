4a2e1bb2284afe0840ea31088f094a2447a488a7
Naman Sinha <aman.sinha5224@gmail.com>
2025-07-26 12:24:49 +0530

    add short hand if-else

--- 09_Conditional.py
+++ 09_Conditional.py
@@ -29,3 +29,16 @@ else:
         print("Number is greater than 999")
     elif(num == 999):
         print("Special Number")
+
+
+
+# Short-Hand if-else
+
+a = 330
+b = 3303
+
+print('A') if a > b else print("=") if (a == b) else print("B")
+
+c = 9 if a > b else 0
+
+print(c)
