a3a8fe89e2444852115aa9afade47f26bc703e3a
Naman Sinha <aman.sinha5224@gmail.com>
2025-08-03 23:19:04 +0530

    add multi processing

---
+++ 43_MultiProcessing.py
@@ -0,0 +1,32 @@
+'''
+Multi Processing:-
+    - Multiprocessing is a Python module that provides a simple way to run multiple processes in parallel. It allows you to take advantage of multiple cores or processors on your system and can significantly improve the performance of your code. In this repl, we'll take a closer look at the multiprocessing module and its various functions and how they can be used in Python.
+'''
+
+import multiprocessing
+import requests
+import concurrent.futures
+
+def downloadFile(url, name):
+    print(f"Started Downloading {name}")
+    response = requests.get(url)
+    open(f"files/file{name}.jpg", "wb").write(response.content)
+    print(f"Finished Downloading {name}")
+
+if __name__ == "__main__":
+    url = "https://picsum.photos/2000/3000"    # pros = []
+
+    # for i in range(5):
+    #     p = multiprocessing.Process(target=downloadFile, args=[url, i])
+    #     p.start()
+    #     pros.append(p)
+
+    # for p in pros:
+    #     p.join()
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        l1 = [url for i in range(60)]
+        l2 = [i for i in range(60)]
+        results = executor.map(downloadFile, l1, l2)
+        for r in results:
+            print(r)
