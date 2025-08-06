334f3b8fa04d0740ffdccdc9d93997f07519b157
Naman Sinha <aman.sinha5224@gmail.com>
2025-08-06 20:40:03 +0530

    add notify app

---
+++ Practice Projects/Notify_Me.py
@@ -0,0 +1,16 @@
+import time
+from plyer import notification
+
+interval = float(input("Enter Interval for Notification (in min.): "))
+
+count = 1
+while True:
+    notification.notify(
+        title='Reminder!',
+        message='Take a break or stretch a bit and drink some water.',
+        app_name='FocusApp',
+        timeout=10  # seconds the notification stays on screen
+    )
+    print(f"Reminder {count}")
+    count += 1
+    time.sleep(interval*60)  # wait 30 minutes before showing next notification
